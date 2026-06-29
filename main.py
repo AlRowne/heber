3e2043713e54e8c74f983178f31985cb9519e5af
AlRowne <aroth@mailbox.org>
2026-06-29 14:41:48 +0200

    feat: add CLI support for entering exercises with Typer

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+import typer
+
 from analysis import group_by_exercise, progress_for_exercise
 from loader import load_sets
 from plot import plot_progress
 
 
-def main():
-    workout_sets = load_sets("liftosaur.csv")
+def main(exercise: str, csv_path: str = "liftosaur.csv"):
+    workout_sets = load_sets(csv_path)
     workout_dict = group_by_exercise(workout_sets)
+    progress = progress_for_exercise(workout_dict, exercise)
+    # rdl_progress = progress_for_exercise(workout_dict, "Romanian Deadlift, Barbell")
+    # for day, e1rm in rdl_progress.items():
+    #     print(f"Date: {day.date()}, RDL e1RM: {e1rm}")
 
-    rdl_progress = progress_for_exercise(workout_dict, "Romanian Deadlift, Barbell")
-    for day, e1rm in rdl_progress.items():
-        print(f"Date: {day.date()}, RDL e1RM: {e1rm}")
-
-    plot_progress(rdl_progress, "RDL Progress")
+    plot_progress(progress, f"{exercise} Progress")
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
