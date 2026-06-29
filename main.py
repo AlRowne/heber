ebd415a7cc83f67077280558636f27c2737bda47
AlRowne <aroth@mailbox.org>
2026-06-29 14:02:08 +0200

    feat: implement basic e1rm plot with matplotlib

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from analysis import group_by_exercise, progress_for_exercise
 from loader import load_sets
+from plot import plot_progress
 
 
 def main():
@@ -9,6 +10,8 @@ def main():
     rdl_progress = progress_for_exercise(workout_dict, "Romanian Deadlift, Barbell")
     for day, e1rm in rdl_progress.items():
         print(f"Date: {day.date()}, RDL e1RM: {e1rm}")
+
+    plot_progress(rdl_progress, "RDL Progress")
 
 
 if __name__ == "__main__":
---
+++ plot.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+
+def plot_progress(progress_dict: dict[datetime, float], title: str):
+    x = list(progress_dict.keys())
+    y = list(progress_dict.values())
+
+    plt.plot(x, y)  # type: ignore[arg-type]
+    plt.title(title)
+    plt.xlabel("Date")
+    plt.ylabel("e1RM")
+    plt.show()
