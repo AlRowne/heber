bda48fe95b2f9395e3c783d6587c06f78a42e8a3
AlRowne <aroth@mailbox.org>
2026-07-01 09:37:39 +0200

    refactor(plot): added tokyo-night style and switched to fig, ax style

--- plot.py
+++ plot.py
@@ -2,13 +2,38 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
+TOKYO_NIGHT = {
+    "figure.facecolor": "#1a1b26",
+    "axes.facecolor": "#1a1b26",
+    "axes.edgecolor": "#414868",
+    "axes.labelcolor": "#c0caf5",
+    "axes.titlecolor": "#c0caf5",
+    "text.color": "#c0caf5",
+    "xtick.color": "#c0caf5",
+    "ytick.color": "#c0caf5",
+    "grid.color": "#414868",
+    "grid.alpha": 0.3,
+}
+plt.rcParams.update(TOKYO_NIGHT)  # type: ignore[arg-type]
+
 
 def plot_progress(progress_dict: dict[datetime, float], title: str):
+    # plt.plot(x, y, marker="o", color="#7aa2f7", markerfacecolor="#bb9af7")  # type: ignore[arg-type]
+    # plt.title(title)
+    # plt.xlabel("Date")
+    # plt.ylabel("e1RM (kg)")
+    # plt.grid(True)
+    # plt.show()
+
     x = list(progress_dict.keys())
     y = list(progress_dict.values())
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(x, y, marker="o", color="#7aa2f7", markerfacecolor="#bb0af7", linewidth=2)  # type: ignore
+    ax.set_title(title)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("e1RM (kg)")
+    ax.grid(True)
 
-    plt.plot(x, y)  # type: ignore[arg-type]
-    plt.title(title)
-    plt.xlabel("Date")
-    plt.ylabel("e1RM (kg)")
+    fig.autofmt_xdate()
+    fig.tight_layout()
     plt.show()
